a91273dcd73f0b0b850dbbd0838f8edbc165de63
constup <16638173+constup@users.noreply.github.com>
2022-01-19 00:49:54 +0100

    Added Square object.

---
+++ src/Example/Square.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Example;
+
+class Square implements MathShapeInterface, TwoDimensionsInterface
+{
+    public function doSomethingWithMathShape(): void
+    {
+        // TODO: Implement doSomethingWithMathShape() method.
+    }
+
+    public function doSomethingWithTwoDimensionalObject(): void
+    {
+        // TODO: Implement doSomethingWithTwoDimensionalObject() method.
+    }
+}
